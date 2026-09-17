672c2f593ed5783ec8f7de07c7b6b2e3dc87fc84
user <shushagei@gmail.com>
2026-09-17 17:58:32 +0300

    Добавил в main strcat

--- first_half_strcmp/main.cpp
+++ first_half_strcmp/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+char* strcat(char* dest, const char* src) {
+    char* ptr = dest;
 
+    while (*ptr != '\0') {
+        ptr++;
+    }
+
+    while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+
+    *ptr = '\0';
+
+    return dest;
+}
 
 
 
