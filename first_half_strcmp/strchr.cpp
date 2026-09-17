3d1b770ef91b3c753952d1283eefb65e90d249b0
snickerd <78643146+snickerd@users.noreply.github.com>
2026-09-17 17:56:24 +0300

    Update strchr.cpp

--- first_half_strcmp/strchr.cpp
+++ first_half_strcmp/strchr.cpp
@@ -3,11 +3,6 @@
 const char* my_strchr(const char* str, int ch);
 
 int main(){
-    const char *str = "hello world\n";
-
-    std::cout << my_strchr(str, 'o');
-    std::cout << my_strchr(str, 'l');
-
     return 0;
 }
 
